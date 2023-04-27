6989b29f32ccf109f613ebb7ee8ef15bb4b6be6e
Julien <julien.perbet@etu.univ-lyon1.fr>
2023-04-27 16:58:28 +0200

    color gamer

--- src/main/java/gui/PieceGui.java
+++ src/main/java/gui/PieceGui.java
@@ -29,9 +29,7 @@ public class PieceGui extends ImageView implements CheckersPieceGui {
 	@Override
 	public boolean hasSameColorAsGamer(PieceSquareColor gamerColor) {
 
-		// ToDo Atelier 2, utile pour Atelier 4
-
-		return false; // A changer 
+		return this.color.equals(gamerColor);
 	}
 
 }
